bbe2a4b11a328c5d6bfb2b4fb1a969dc619ce7d5
haford <56978762+haford@users.noreply.github.com>
2026-04-13 00:06:31 +0200

    debug: remove all CSP headers to test Firestore connectivity

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,5 @@
 import type { NextConfig } from "next";
 
-const csp = [
-  "default-src 'self'",
-  "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://*.googleapis.com",
-  "style-src 'self' 'unsafe-inline'",
-  "connect-src 'self' https://*.googleapis.com https://*.firebase.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://identitytoolkit.googleapis.com",
-  "img-src 'self' data: https:",
-  "frame-src https://timetracker-hans.firebaseapp.com https://accounts.google.com",
-  "font-src 'self' data:",
-].join("; ");
-
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: csp,
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
